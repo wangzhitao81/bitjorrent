0ac32882c83f2fdfb6c92c40ac7cbc6df0b9bbaa
Benjamin Weintraub <ben@benjamins-mbp.gateway.pace.com>
2015-07-19 18:21:20 -0700

    bencode is working without lists

--- src/bitjorrent/bencode.clj
+++ src/bitjorrent/bencode.clj
@@ -8,64 +8,77 @@
 (defn parse-list
   [])
 
-; could be recursive
-;; (defn parse-dict
-;;   "parse elements after d, but before e"
-;;   [stream]
-;;   {parse-key parse-value})
-
-;; not recursive
 (defn parse-int
-  ([stream]
-   (parse-int stream ""))
-  ([stream acc]
-   (let [n (first stream)]
-    (if (and (not (= n \e)) (not (= n \:)))
-      (recur (rest stream) (str acc n))
-      [stream acc]))))
+  ([stream] ;; this arity is for returning just the number without a struct
+   (loop [[n & more] stream
+          acc ""]
+     (if (and (not (= n \e)) (not (= n \:)))
+       (recur more (str acc n))
+       (do
+;;         (println "parse int:" acc) ;; to be removed, only for debugging
+         [more (read-string acc)]))))
+  ([stream struct] ;; this arity is for returning the number included in the provided struct
+   (let [[tmp_stream num] (parse-int stream)]
+     [tmp_stream (conj struct num)])))
 
 ;; not recursive
 (defn parse-bytes
-  [stream]
-  (let [[s num] (parse-int stream)]
-    (loop [newstream (rest s) ;; rest to get rid of colon
-           i (dec (read-string num)) ;; dec to get rid of colon
-           acc ""]
-      (if (> i 0)
-        (recur (rest newstream) (dec i) (str acc (first newstream)))
-        [(rest newstream) (str acc (first newstream))]))))
+  ([stream]
+   (let [[tmp_stream num] (parse-int stream)]
+     (loop [newstream tmp_stream
+            i (dec num) ;; dec to get rid of colon
+            acc ""]
+       (if (> i 0)
+         (recur (rest newstream) (dec i) (str acc (first newstream)))
+         (let [result (str acc (first newstream))]
+;;           (println "parse bytes:" result) ;; to be removed, only for debugging
+           [(rest newstream) result])))))
+  ([stream struct]
+   (let [[tmp_stream bytes] (parse-bytes stream)]
+     [tmp_stream (conj struct bytes)])))
+
+(declare parse-token)
+
+(defn parse-pair
+  ([stream]
+   (let [[new_stream key] (parse-token stream)
+         [newer_stream value] (parse-token new_stream)]
+;;     (println "key:" key)
+;;     (println "value:" value)
+     [newer_stream [key value]])))
+
+(defn parse-dict
+  ([stream]
+   (loop [[tmp_stream kv_pair] (parse-pair stream) 
+          acc {}]
+     (let [new_acc (conj acc kv_pair)]
+       (if (not (= (first tmp_stream) \e))
+         (recur (parse-pair tmp_stream) new_acc)
+         [(rest tmp_stream) new_acc]))))
+  ([stream struct]
+   (let [[new_stream new_struct] (parse-dict stream)]
+     [new_stream (conj struct new_struct)])))
 
 (defn parse-token
-  [stream struct]
-  (let [first-char (first stream)]
-    (println first-char)
-    (cond
-      ;; recur
-      (= first-char \d) (let [[newstream key] (parse-token (rest stream) struct)
-                              [newerstream value] (parse-token (rest newstream) struct)]
-                          [newerstream (conj struct {key value})])
-
-      ;; don't recur
-      (= first-char \i) (let [[newstream num] (parse-int (rest stream) struct)]
-                          (println num)
-                          [newstream num])
-      
-      ;; don't recur
-      (re-matches #"\d" (str first-char)) (let [[newstream bytes] (parse-bytes stream)]
-                                            (println bytes)
-                                            (println "first in newstream:" (first newstream))
-                                            [newstream bytes])
-      :else (when (not (empty? stream))
-              (recur (rest stream) struct)))))
+  ([stream]
+   (let [[first_char & more] stream]
+     (cond
+       (= first_char \d) (parse-dict more)
+       (= first_char \i) (parse-int more)
+       (re-matches #"\d" (str first_char)) (parse-bytes stream)
+       :else (println "This is bad, should not be here"))))
+  ([stream struct]
+   (let [[tmp_stream token] (parse-token stream)]
+     [tmp_stream (conj struct token)])))
 
 (defn parse-tokens
   ([stream]
    (parse-tokens stream ()))
   ([stream struct]
-   (if (not (empty? stream))
-     (let [[newstream newstruct] (parse-token stream struct)]
-       (recur newstream newstruct))
-     struct)))
+   (let [[new_stream new_struct] (parse-token stream struct)]
+     (if (not (empty? new_stream))
+       (recur new_stream new_struct)
+       new_struct))))
 
 (defn file-stream
   [uri]
@@ -79,6 +92,8 @@
 
 (defn decode
   [path]
-  (let [char-stream (map char (file-stream path))]
-    (println "results: " (parse-tokens char-stream))))
+  (let [char-stream (map char (file-stream path))
+        results (parse-tokens char-stream)]
+    (println "results:" results)
+    results))
 
